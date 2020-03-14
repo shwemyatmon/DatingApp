bbc5122f40b7d1747565a961d130656d359cb604
nyeinmyatmon21@gmail.com <nyeinmyatmon21@gmail.com>
2020-03-14 18:43:00 +0800

    End of Section 6

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap';
+
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -18,6 +20,7 @@ import { ErrorInterceptorProvider } from './_services/error.Interceptor';
       RegisterComponent
    ],
    imports: [
+      BsDropdownModule.forRoot(),
       BrowserModule,
       HttpClientModule,
       FormsModule
