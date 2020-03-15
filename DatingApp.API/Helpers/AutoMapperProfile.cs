b79a39f25e9660dc53003719c024ca123a631676
nyeinmyatmon21@gmail.com <nyeinmyatmon21@gmail.com>
2020-03-15 17:41:26 +0800

    End of Section 8

---
+++ DatingApp.API/Helpers/AutoMapperProfile.cs
@@ -0,0 +1,27 @@
+using System.Linq;
+using AutoMapper;
+using DatingApp.API.DTOs;
+using DatingApp.API.Models;
+
+namespace DatingApp.API.Helpers
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<User, UserForListDto>()
+            .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(
+                src => src.Photos.FirstOrDefault(u => u.IsMain).Url))
+            .ForMember(dest => dest.Age, opt => opt.MapFrom(
+                src => src.DateofBirth.CalcaulateAge()));
+            CreateMap<User, UserForDetailDto>()
+             .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(
+                src => src.Photos.FirstOrDefault(u => u.IsMain).Url))
+                 .ForMember(dest => dest.Age, opt => opt.MapFrom(
+                src => src.DateofBirth.CalcaulateAge()));
+            CreateMap<Photo, PhotoForDetailedDto>();
+
+        }
+
+    }
+}
