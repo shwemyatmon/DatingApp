dd9459848897d152b268fa8974e8c95d3452edc7
nyeinmyatmon21@gmail.com <nyeinmyatmon21@gmail.com>
2020-04-20 12:40:14 +0800

    End of Section 17

--- DatingApp.API/Data/DatingRepository.cs
+++ DatingApp.API/Data/DatingRepository.cs
@@ -46,14 +46,14 @@ namespace DatingApp.API.Data
 
         public async Task<User> GetUser(int id)
         {
-            var user=await _context.Users.Include(p => p.Photos)
+            var user=await _context.Users
             .FirstOrDefaultAsync(u => u.Id==id);
             return user;
         }
 
         public async Task<PagedList<User>> GetUsers(UserParams userParams)
         {
-            var users=_context.Users.Include(p => p.Photos).OrderByDescending( u => u.LastActive).AsQueryable();
+            var users=_context.Users.OrderByDescending( u => u.LastActive).AsQueryable();
 
              users =users.Where(u => u.Id != userParams.UserId);
 
@@ -92,8 +92,6 @@ namespace DatingApp.API.Data
 
         private async Task<IEnumerable<int>> GetUserLikes(int id,bool likers){
             var user= await _context.Users
-            .Include(u => u.Likers)
-            .Include(u => u.Likees)
             .FirstOrDefaultAsync(u => u.Id== id);
 
             if(likers){
@@ -124,10 +122,7 @@ namespace DatingApp.API.Data
 
         public async Task<PagedList<Message>> GetMessagesForUser(MessageParams messageParams)
         {
-            var messages = _context.Messages.Include(u => u.Sender)
-            .ThenInclude(p => p.Photos)
-            .Include(u => u.Recipient)
-            .ThenInclude(p => p.Photos).AsQueryable();
+            var messages = _context.Messages.AsQueryable();
 
             switch(messageParams.MessageContainer){
                 case "Inbox": messages = messages.Where(m => m.RecipientId == messageParams.UserId && m.RecipentDeleted==false);
@@ -146,10 +141,7 @@ namespace DatingApp.API.Data
 
         public async Task<IEnumerable<Message>> GetMessgeThread(int userId, int recipientID)
         {
-             var messages = await _context.Messages.Include(u => u.Sender)
-            .ThenInclude(p => p.Photos)
-            .Include(u => u.Recipient)
-            .ThenInclude(p => p.Photos)
+             var messages = await _context.Messages
             .Where( m => m.RecipientId == userId && m.SenderId == recipientID && m.RecipentDeleted== false
             || m.RecipientId == recipientID && m.SenderId == userId && m.SenderDeleted ==false)
             .OrderByDescending(m => m.MessageSent)
--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -32,11 +32,25 @@ namespace DatingApp.API
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public void ConfigureDevelopmentServices(IServiceCollection services){
+               services.AddDbContext<DataContext>(x => {     
+                x.UseLazyLoadingProxies();
+                x.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+               });
+            ConfigureServices(services);
+        }
+
+         public void ConfigureProductionServices(IServiceCollection services){
+              services.AddDbContext<DataContext>(x => {     
+                x.UseLazyLoadingProxies();
+                x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+               });
+            ConfigureServices(services);
+        }
+
+             public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(x => x.UseSqlite
-            (Configuration.GetConnectionString("DefaultConnection")));
+          
             services.AddControllers().AddNewtonsoftJson(opt => {
                 opt.SerializerSettings.ReferenceLoopHandling = 
                 Newtonsoft.Json.ReferenceLoopHandling.Ignore;
@@ -58,6 +72,12 @@ namespace DatingApp.API
             });
             services.AddAutoMapper(typeof(DatingRepository).Assembly);
             services.AddScoped<LogUserActivity>();
+
+            services.AddLogging(logging =>
+            {
+                logging.AddConsole();
+                logging.AddDebug();
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -93,9 +113,13 @@ namespace DatingApp.API
 
             app.UseAuthorization();
 
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapFallbackToController("Index","Fallback");
             });
         }
     }
